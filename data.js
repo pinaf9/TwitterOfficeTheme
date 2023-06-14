4c89a1059662ae0802b041d433356cd3fd08314e
Francheska Pina <pinaf9@gmail.com>
2023-06-13 23:48:37 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,59 @@
+export const tweetsData = [   
+    {
+        handle: `@crazy_cat_lady`,
+        profilePic: `images/ange1.png`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Sometimes the clothes at Gap Kids are too flashy, 
+        so I’m forced to go to the American Girl store and order clothes for large colonial dolls.`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@TheKidsCallMeBoss`,
+        profilePic: `images/MichaelScott.png`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me 💖`,
+        replies: [
+                  {
+                handle: `@Kevin2hot `,
+                profilePic: `images/kev.jpeg`,
+                tweetText: `I just want to lie on the beach and eat hot dogs. That’s all I’ve ever wanted. 🏖️🌭`,
+            },
+                  {
+                handle: `@date_me 🩷`,
+                profilePic: `images/kellyK.jpeg`,
+                tweetText: `I talk a lot, so I’ve learned to tune myself out.🙊`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@Cornell4life ✅`,
+        profilePic: `images/andy.webp`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `I wish there was a way to know you're in the good old days before you've actually left them.`,
+        replies: [
+            {
+                handle: `@spacebound 🚀`,
+                profilePic: `images/Dwight.jpeg`,
+                tweetText: `People underestimate the power of nostalgia. 
+                Nostalgia is truly one of the greatest human weaknesses, second only to the neck. ⌛️`,
+            },
+            {
+                handle: `@ takenbyWine`,
+                profilePic: `images/Meredith1.jpg`,
+                tweetText: `For my new year's resolution, I gave up drinking...during the week.🍸`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
